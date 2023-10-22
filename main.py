cb7d24d64ac3852b88dafccd40bcd373ea7b1279
Werds <santiagoandresxbox@gmail.com>
2023-10-21 20:58:34 -0500

    Español a ingles Y ya se puede cargar las imagenes dicom

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ class DICOMViewerApp(Tk):
 		self.grid_rowconfigure(1, weight=8)
 		self.grid_columnconfigure(1, weight=1)
 
-		# Nose como conectar la opcion escogida en la BarraSuperior con el texto del PanelDerecho
+		# Nose como conectar la opcion escogida en la MainBar con el texto del Panel
 		self.opcion_escogida = StringVar(self)
 
 		# La barra que esta completamente arriba (La que dice Archivo, Editar, Ayuda...)
-		self.BarraMenu = BarraMenu(self)
+		self.MenuBar = MenuBar(self)
 
 		# LA barra donde vamos a poner todas las herramientas (Frame azul)
-		self.BarraSuperior = BarraSuperior(self)
+		self.MainBar = MainBar(self)
 
 		# Frame donde vamos a poner las opciones/parametros de las herramientas (Frame verde)
-		self.PanelDerecho = PanelDerecho(self)
+		self.Panel = Panel(self)
 
 		# Donde se va a visualizar los dicom (Frame Rojo)
-		self.Visualizador = Visualizador(self)
+		self.ImageViewer = ImageViewer(self)
 
 		self.mainloop()
 
 
-class BarraMenu(Menu):
+class MenuBar(Menu):
 	def __init__(self, raiz):
 		super().__init__(raiz)
 		raiz.config(menu=self)
@@ -80,12 +80,12 @@ class BarraMenu(Menu):
 		self.add_cascade(label="Ayuda", menu=menu_ayuda)
 
 
-class BarraSuperior(Frame):
+class MainBar(Frame):
 	def __init__(self, raiz):
-		super().__init__(raiz, style="BarraSuperior.TFrame")
-		Style().configure('BarraSuperior.TFrame', background='lightblue')
+		super().__init__(raiz, style="MainBar.TFrame")
+		Style().configure('MainBar.TFrame', background='Gray')
 		self.grid(column=0, row=0, columnspan=2, sticky="nsew")
-		# todo: nose como conectar la opcion_escogida para que el PanelDerecho muestre lo que se escogio
+		# todo: nose como conectar la opcion_escogida para que el Panel muestre lo que se escogio
 		self.opcion_escogida = self.master.opcion_escogida
 		self.contenido()
 
@@ -111,7 +111,7 @@ class BarraSuperior(Frame):
 		boton_medicion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
 		# Boton visualizacion
-		boton_visualizacion = Menubutton(self, text="Herramientas medicion")
+		boton_visualizacion = Menubutton(self, text="Herramientas visualizacion")
 		menu = Menu(boton_visualizacion, tearoff=0)
 		opciones_visualizacion = ("Opcion c", "Opcion d", "etc...")
 		for opcion in opciones_visualizacion:
@@ -120,7 +120,7 @@ class BarraSuperior(Frame):
 		boton_visualizacion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
 		# Boton dibujo
-		boton_dibujo = Menubutton(self, text="Herramientas medicion")
+		boton_dibujo = Menubutton(self, text="Herramientas de dibujo")
 		menu = Menu(boton_dibujo, tearoff=0)
 		opciones_dibujo = ("Opcion e", "Opcion f", "etc...")
 		for opcion in opciones_dibujo:
@@ -129,7 +129,7 @@ class BarraSuperior(Frame):
 		boton_dibujo.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
 		# Boton basicas
-		boton_basicas = Menubutton(self, text="Herramientas medicion")
+		boton_basicas = Menubutton(self, text="Herramientas basicas")
 		menu = Menu(boton_basicas, tearoff=0)
 		opciones_basicas = ("Opcion g", "Opcion h", "etc...")
 		for opcion in opciones_basicas:
@@ -138,11 +138,11 @@ class BarraSuperior(Frame):
 		boton_basicas.pack(side=LEFT, padx=(2, 4), pady=3, fill="both", expand=True)
 
 
-class PanelDerecho(Frame):
+class Panel(Frame):
 	# todo: Este panel esta completamente incompleto
 	def __init__(self, raiz):
-		super().__init__(raiz, style='PanelDerecho.TFrame')
-		Style().configure('PanelDerecho.TFrame', background='lightgreen')
+		super().__init__(raiz, style='Panel.TFrame')
+		Style().configure('Panel.TFrame', background='lightgray')
 		self.opcion_escogida = self.master.opcion_escogida
 		self.grid(column=1, row=1, pady=3, padx=3, sticky="nsew")
 
@@ -157,17 +157,67 @@ class PanelDerecho(Frame):
 		_args = args
 
 
-class Visualizador(Frame):
+class ImageViewer(Frame):
 	def __init__(self, raiz):
-		super().__init__(raiz, style='Visualizador.TFrame')
-		Style().configure('Visualizador.TFrame', background='lightpink')
+		super().__init__(raiz, style='ImageViewer.TFrame')
+		Style().configure('ImageViewer.TFrame', background='white')
 		self.grid(column=0, row=1, pady=3, padx=3, sticky="nsew")
 		self.contenido()
 
 	def contenido(self):
-		# boton_cargar = tk.Button(self, text="buscar")
-		# boton_cargar.pack(expand=True)
-		pass
+		self.folder_path = StringVar()
+		self.current_slice = 0
+
+		folder_label = Label(self, text="Carpeta DICOM:")
+		folder_label.grid(row=0, column=0, sticky="w")
+
+		self.folder_entry = Entry(self, textvariable=self.folder_path)
+		self.folder_entry.grid(row=0, column=1, sticky="ew")
+
+		load_button = Button(self, text="Cargar DICOM", command=self.load_dicom_files)
+		load_button.grid(row=0, column=2, sticky="e")
+
+		prev_button = Button(self, text="Anterior Slice", command=self.prev_slice)
+		prev_button.grid(row=1, column=0, sticky="w")
+
+		next_button = Button(self, text="Siguiente Slice", command=self.next_slice)
+		next_button.grid(row=1, column=2, sticky="e")
+
+		self.image_label = Label(self)
+		self.image_label.grid(row=2, column=0, columnspan=3)
+
+	def load_dicom_files(self):
+		folder_path = self.folder_path.get()
+		dicom_files = [f for f in os.listdir(folder_path) if f.endswith('.dcm')]
+		dicom_files.sort()
+		self.dicom_data = [pydicom.dcmread(os.path.join(folder_path, f)) for f in dicom_files]
+
+		# Mostrar el primer slice
+		self.current_slice = 0
+		self.display_slice()
+
+	def display_slice(self):
+		if not hasattr(self, 'dicom_data') or not self.dicom_data:
+			return
+
+		dicom_slice = self.dicom_data[self.current_slice]
+		pixel_array = dicom_slice.pixel_array
+		image = Image.fromarray(pixel_array)
+		photo = ImageTk.PhotoImage(image=image)
+
+		self.image_label.config(image=photo)
+		self.image_label.image = photo
+
+	def prev_slice(self):
+		if self.current_slice > 0:
+			self.current_slice -= 1
+			self.display_slice()
+
+	def next_slice(self):
+		if self.current_slice < len(self.dicom_data) - 1:
+			self.current_slice += 1
+			self.display_slice()
+
 
 
 if __name__ == "__main__":
