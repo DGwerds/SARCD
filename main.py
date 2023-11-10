ea0cbcde1a80d2798c65142dfa06d3d09313b456
Werds <santiago2211799@correo.uis.edu.co>
2023-11-10 00:04:20 -0500

    GUI changes
    - cambie los labels por canvas (son mas convenientes)
    - Arregle interfaz grafica
    - solo muestra una imagen
    - cambie nombres de variables
    - elimine codigo obsoleto

--- main.py
+++ main.py
@@ -1,227 +1,184 @@
 import os
 from tkinter import *
 from tkinter.ttk import *
-import tkinter as tk
 from PIL import Image, ImageTk
 import matplotlib.pyplot as plt
-import pydicom
-# pydicom es la libreria que nos va a ayudar con muchas cosas
-# todo: Solo me encargue de la parte visual, no hay ningun tipo de funcionalidad agregada
+import pydicom as dcm
 
 
 class DICOMViewerApp(Tk):
-	# iniciar la ventana (En la mayoria de partes voy a hacer referencia a esta ventana con el nombre "raiz")
-	def __init__(self):
-		super().__init__()
-		self.title("MOCID")
-		self.geometry("900x550")
+    def __init__(self):
+        super().__init__()
+        self.title("MOCID")
+        self.geometry("1400x550")
 
-		self.grid_rowconfigure(0, weight=1)
-		self.grid_columnconfigure(0, weight=8)
+        self.grid_rowconfigure(index=0, minsize=50)
+        self.grid_rowconfigure(index=1, weight=1)
+        self.grid_rowconfigure(index=2, weight=1)
+        self.grid_rowconfigure(index=3, weight=1)
+        self.grid_rowconfigure(index=4, weight=1)
 
-		self.grid_rowconfigure(1, weight=8)
-		self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(index=0, weight=1)
+        self.grid_columnconfigure(index=1, weight=1)
+        self.grid_columnconfigure(index=2, weight=1)
+        self.grid_columnconfigure(index=3, weight=1)
+        self.grid_columnconfigure(index=4, minsize=250)
 
-		# Nose como conectar la opcion escogida en la MainBar con el texto del Panel
-		self.opcion_escogida = StringVar(self)
+        self.opcion_escogida: StringVar = StringVar(self)
 
-		# La barra que esta completamente arriba (La que dice Archivo, Editar, Ayuda...)
-		self.MenuBar = MenuBar(self)
-
-		# LA barra donde vamos a poner todas las herramientas (Frame azul)
-		self.MainBar = MainBar(self)
-
-		# Frame donde vamos a poner las opciones/parametros de las herramientas (Frame verde)
-		self.Panel = Panel(self)
-
-		# Donde se va a visualizar los dicom (Frame Rojo)
-		self.ImageViewer = ImageViewer(self)
-
-		self.mainloop()
+        self.menu_bar: Menu = MenuBar(self)
+        self.main_bar: Frame = MainBar(self)
+        self.panel: Frame = Panel(self)
+        self.image_viewer: Frame = ImageViewer(self)
 
 
 class MenuBar(Menu):
-	def __init__(self, raiz):
-		super().__init__(raiz)
-		raiz.config(menu=self)
-		self.contenido()
+    def __init__(self, root_window):
+        super().__init__(root_window)
+        root_window.config(menu=self)
+        self.create_menu()
 
-	def contenido(self):
-		# Menu desplegable de "Archivo"
-		menu_archivo = Menu(self, tearoff=0)
-		menu_archivo.add_command(label="Nuevo")
-		menu_archivo.add_command(label="Abrir", accelerator="Ctrl+N")
+    def create_menu(self):
+        menu_archivo = Menu(self, tearoff=0)
+        menu_archivo.add_command(label="Nuevo")
+        menu_archivo.add_command(label="Abrir", accelerator="Ctrl+N")
 
-		def guardar():
-			print("Jajajaj, archivo ''guardado''")
+        def guardar():
+            print("Jajajaj, archivo ''guardado''")
 
-		menu_archivo.add_command(label="Guardar", accelerator="Ctrl+G", command=guardar)
-		menu_archivo.add_separator()
+        menu_archivo.add_command(label="Guardar", accelerator="Ctrl+G", command=guardar)
+        menu_archivo.add_separator()
 
-		menu_archivo.add_command(label="Cerrar", accelerator="Alt+F4", command=self.master.destroy)
-		self.add_cascade(label="Archivo", menu=menu_archivo)
+        menu_archivo.add_command(label="Cerrar", accelerator="Alt+F4", command=self.master.destroy)
+        self.add_cascade(label="Archivo", menu=menu_archivo)
 
-		# Menu desplegable de "Editar"
-		menu_editar = Menu(self, tearoff=0)
-		menu_editar.add_command(label="Cortar", state=DISABLED)
-		menu_editar.add_command(label="Copiar")
-		menu_editar.add_command(label="Pegar")
-		self.add_cascade(label="Editar", menu=menu_editar)
+        menu_editar = Menu(self, tearoff=0)
+        menu_editar.add_command(label="Cortar", state=DISABLED)
+        menu_editar.add_command(label="Copiar")
+        menu_editar.add_command(label="Pegar")
+        self.add_cascade(label="Editar", menu=menu_editar)
 
-		# Menu desplegable de "Ayuda"
-		menu_ayuda = Menu(self, tearoff=0)
-		menu_ayuda.add_command(label="Ayudameeee", state=DISABLED)
-		menu_ayuda.add_separator()
+        menu_ayuda = Menu(self, tearoff=0)
+        menu_ayuda.add_command(label="Ayudameeee", state=DISABLED)
+        menu_ayuda.add_separator()
 
-		def funcion_de_prueba():
-			menu_ayuda.entryconfig(0, state=NORMAL)
+        def funcion_de_prueba():
+            menu_ayuda.entryconfig(index=0, state=NORMAL)
 
-		menu_ayuda.add_command(label="Activar boton ayudameeee", command=funcion_de_prueba)
-		self.add_cascade(label="Ayuda", menu=menu_ayuda)
+        menu_ayuda.add_command(label="Activar boton ayudameeee", command=funcion_de_prueba)
+        self.add_cascade(label="Ayuda", menu=menu_ayuda)
 
 
 class MainBar(Frame):
-	def __init__(self, raiz):
-		super().__init__(raiz, style="MainBar.TFrame")
-		Style().configure('MainBar.TFrame', background='Gray')
-		self.grid(column=0, row=0, columnspan=2, sticky="nsew")
-		# todo: nose como conectar la opcion_escogida para que el Panel muestre lo que se escogio
-		self.opcion_escogida = self.master.opcion_escogida
-		self.contenido()
+    def __init__(self, root_window):
+        super().__init__(root_window, style="MainBar.TFrame")
+        Style().configure(style='MainBar.TFrame', background='lightblue')
+        self.grid(column=0, row=0, columnspan=5, sticky="nsew")
+        self.create_widgets()
 
-	def contenido(self):
-		# configurar boton segmentacion
-		boton_segmentacion = Menubutton(self, text="Herramientas segmentacion")
-		menu = Menu(boton_segmentacion, tearoff=0)
+    def create_widgets(self):
+        boton_segmentacion = Menubutton(self, text="Herramientas segmentacion")
+        menu = Menu(boton_segmentacion, tearoff=0)
+        menu.add_radiobutton(label="opcionx", value="opcionx", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="opciony", value="opciony", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="opcionz", value="opcionz", variable=self.master.opcion_escogida)
+        boton_segmentacion["menu"] = menu
+        boton_segmentacion.pack(side=LEFT, padx=(4, 2), pady=3, fill="both", expand=True)
 
-		opciones_segmentacion = ("Opcion 1", "Opcion 2", "etc...")
-		for opcion in opciones_segmentacion:
-			menu.add_radiobutton(label=opcion, value=opcion, variable=self.opcion_escogida)
-		boton_segmentacion["menu"] = menu
-		boton_segmentacion.pack(side=LEFT, padx=(4, 2), pady=3, fill="both", expand=True)
+        boton_medicion = Menubutton(self, text="Herramientas medicion")
+        menu = Menu(boton_medicion, tearoff=0)
+        menu.add_radiobutton(label="Opcion a", value="Opcion a", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="Opcion b", value="Opcion b", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="Opcion c", value="opcion c", variable=self.master.opcion_escogida)
+        boton_medicion["menu"] = menu
+        boton_medicion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
-		# boton de medicion
-		boton_medicion = Menubutton(self, text="Herramientas medicion")
-		menu = Menu(boton_medicion, tearoff=0)
+        boton_visualizacion = Menubutton(self, text="Herramientas visualizacion")
+        menu = Menu(boton_visualizacion, tearoff=0)
+        menu.add_radiobutton(label="Opcion c", value="Opcion c", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="Opcion d", value="Opcion d", variable=self.master.opcion_escogida)
+        boton_visualizacion["menu"] = menu
+        boton_visualizacion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
-		opciones_medicion = ("Opcion a", "Opcion b", "etc...")
-		for opcion in opciones_medicion:
-			menu.add_radiobutton(label=opcion, value=opcion, variable=self.opcion_escogida)
-		boton_medicion["menu"] = menu
-		boton_medicion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
+        boton_dibujo = Menubutton(self, text="Herramientas de dibujo")
+        menu = Menu(boton_dibujo, tearoff=0)
+        menu.add_radiobutton(label="Opcion e", value="Opcion e", variable=self.master.opcion_escogida)
+        menu.add_radiobutton(label="Opcion f", value="Opcion f", variable=self.master.opcion_escogida)
+        boton_dibujo["menu"] = menu
+        boton_dibujo.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
 
-		# Boton visualizacion
-		boton_visualizacion = Menubutton(self, text="Herramientas visualizacion")
-		menu = Menu(boton_visualizacion, tearoff=0)
-		opciones_visualizacion = ("Opcion c", "Opcion d", "etc...")
-		for opcion in opciones_visualizacion:
-			menu.add_radiobutton(label=opcion, value=opcion, variable=self.opcion_escogida)
-		boton_visualizacion["menu"] = menu
-		boton_visualizacion.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
-
-		# Boton dibujo
-		boton_dibujo = Menubutton(self, text="Herramientas de dibujo")
-		menu = Menu(boton_dibujo, tearoff=0)
-		opciones_dibujo = ("Opcion e", "Opcion f", "etc...")
-		for opcion in opciones_dibujo:
-			menu.add_radiobutton(label=opcion, value=opcion, variable=self.opcion_escogida)
-		boton_dibujo["menu"] = menu
-		boton_dibujo.pack(side=LEFT, padx=2, pady=3, fill="both", expand=True)
-
-		# Boton basicas
-		boton_basicas = Menubutton(self, text="Herramientas basicas")
-		menu = Menu(boton_basicas, tearoff=0)
-		opciones_basicas = ("Opcion g", "Opcion h", "etc...")
-		for opcion in opciones_basicas:
-			menu.add_radiobutton(label=opcion, value=opcion, variable=self.opcion_escogida)
-		boton_basicas["menu"] = menu
-		boton_basicas.pack(side=LEFT, padx=(2, 4), pady=3, fill="both", expand=True)
+        boton_basicas = Menubutton(self, text="Herramientas basicas")
+        menu = Menu(boton_basicas, tearoff=0)
+        opciones_basicas = ("Opcion g", "Opcion h", "etc...")
+        for i in opciones_basicas:
+            menu.add_radiobutton(label=i, value=i, variable=self.master.opcion_escogida)
+        boton_basicas["menu"] = menu
+        boton_basicas.pack(side=LEFT, padx=(2, 4), pady=3, fill="both", expand=True)
 
 
 class Panel(Frame):
-	# todo: Este panel esta completamente incompleto
-	def __init__(self, raiz):
-		super().__init__(raiz, style='Panel.TFrame')
-		Style().configure('Panel.TFrame', background='lightgray')
-		self.opcion_escogida = self.master.opcion_escogida
-		self.grid(column=1, row=1, pady=3, padx=3, sticky="nsew")
+    def __init__(self, root_window):
+        super().__init__(root_window, style='Panel.TFrame')
+        Style().configure(style='Panel.TFrame', background='lightgreen',  width=300, height=100)
+        self.grid(column=4, row=1, rowspan=4, pady=3, padx=3, sticky="nsew")
 
-		self.label = Label(self, text="")
-		self.label.pack()
-		self.opcion_escogida.trace("w", self.actualizar_label)
-
-	def actualizar_label(self, *args):
-		# Obtener el valor de la variable y actualizar el Label
-		valor = self.opcion_escogida.get()
-		self.label.config(text=f"Herramientas de: {valor}")
-		_args = args
+        self.label = Label(self, text="Panel")
+        self.label.grid(row=0, column=0, padx=2, pady=4, sticky="nsew")
 
 
 class ImageViewer(Frame):
-	def __init__(self, raiz):
-		super().__init__(raiz, style='ImageViewer.TFrame')
-		Style().configure('ImageViewer.TFrame', background='white')
-		self.grid(column=0, row=1, pady=3, padx=3, sticky="nsew")
-		self.contenido()
+    def __init__(self, root_window):
+        super().__init__(root_window, style='ImageViewer.TFrame')
+        Style().configure(style='ImageViewer.TFrame', background='white')
+        self.grid(column=0, row=1, columnspan=4, rowspan=4, pady=3, padx=3, sticky="nsew")
 
-	def contenido(self):
-		self.folder_path = StringVar()
+        # Grid 3x3, en el 2x2 se muestran imagenes, y la otra fila y columna la dejo por si al caso
+        self.grid_rowconfigure(index=0, weight=1)
+        self.grid_columnconfigure(index=0, weight=1)
 
-		self.current_slice = 0
+        self.grid_rowconfigure(index=1, weight=1)
+        self.grid_columnconfigure(index=1, weight=1)
 
-		folder_label = Label(self, text="Carpeta DICOM:")
-		folder_label.grid(row=0, column=0, sticky="w")
+        self.grid_rowconfigure(index=2, weight=0)
+        self.grid_columnconfigure(index=2, weight=0)
 
-		self.folder_entry = Entry(self, textvariable=self.folder_path)
-		self.folder_entry.grid(row=0, column=1, sticky="ew")
+        self.load_dicom_files("series")
 
-		load_button = Button(self, text="Cargar DICOM", command=self.load_dicom_files)
-		load_button.grid(row=0, column=2, sticky="e")
+    def load_dicom_files(self, folder_path):
+        # canvas2 = Canvas(self)
+        # canvas3 = Canvas(self)
+        # canvas4 = Canvas(self)
 
-		prev_button = Button(self, text="Anterior Slice", command=self.prev_slice)
-		prev_button.grid(row=1, column=0, sticky="w")
+        slice_number: int = 0
+        canvas1 = Canvas(self)
 
-		next_button = Button(self, text="Siguiente Slice", command=self.next_slice)
-		next_button.grid(row=1, column=2, sticky="e")
+        img_paths: list[str] = [os.path.join(folder_path, file)
+                                for file in os.listdir(folder_path) if file.endswith(".dcm")]
 
-		self.image_label = Label(self)
-		self.image_label.grid(row=2, column=0, columnspan=3)
+        dicoms_data = [dcm.dcmread(img) for img in img_paths]
 
-	def load_dicom_files(self):
-		self.folder_path.set("series")
-		folder_path = self.folder_path.get()
-		dicom_files = [f for f in os.listdir(folder_path) if f.endswith('.dcm')]
-		dicom_files.sort()
-		self.dicom_data = [pydicom.dcmread(os.path.join(folder_path, f)) for f in dicom_files]
+        pixel_array = dicoms_data[slice_number].pixel_array
 
-		# Mostrar el primer slice
-		self.current_slice = 0
-		self.display_slice()
+        photo = Image.fromarray(pixel_array)
 
-	def display_slice(self):
-		if not hasattr(self, 'dicom_data') or not self.dicom_data:
-			return
+        # resize photo to canvas1 sizes
+        photo = ImageTk.PhotoImage(image=photo)
 
-		dicom_slice = self.dicom_data[self.current_slice]
-		pixel_array = dicom_slice.pixel_array
-		image = Image.fromarray(pixel_array)
-		photo = ImageTk.PhotoImage(image=image)
+        # show image in canvas1
+        canvas1.create_image(0, 0, image=photo, anchor=NW)
+        canvas1.image = photo  # Evita que la imagen se recolecte por el recolector de basura
 
-		self.image_label.config(image=photo)
-		self.image_label.image = photo
+        canvas1.grid(row=0, column=0, columnspan=2, rowspan=2, padx=(4, 2), pady=(4, 2), sticky="nsew")
+        # canvas2.grid(row=0, column=1, padx=(2, 4), pady=(4, 2), sticky="nsew")
+        # canvas3.grid(row=1, column=0, padx=(4, 2), pady=(2, 4), sticky="nsew")
+        # canvas4.grid(row=1, column=1, padx=(2, 4), pady=(2, 4), sticky="nsew")
+        # imagen = imagen.resize((400, 300), Image.ANTIALIAS)  # Ajusta el tamaño de la imagen según sea necesario
+        # imagen_tk = ImageTk.PhotoImage(imagen)
 
-	def prev_slice(self):
-		if self.current_slice > 0:
-			self.current_slice -= 1
-			self.display_slice()
-
-	def next_slice(self):
-		if self.current_slice < len(self.dicom_data) - 1:
-			self.current_slice += 1
-			self.display_slice()
-
+        # Crea un widget Label para mostrar la imagen
+        # label_imagen = Label(self, image=imagen_tk)
 
 
 if __name__ == "__main__":
-	app = DICOMViewerApp()
-	app.mainloop()
+    app = DICOMViewerApp()
+    app.mainloop()
