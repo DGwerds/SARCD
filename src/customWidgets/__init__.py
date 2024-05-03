a9aed07a0c998302a12c8f2885da84a6024696ab
Sanq <santiagoandresxbox@gmail.com>
2024-05-02 19:42:43 -0500

    Quick fix

--- src/customWidgets/__init__.py
+++ src/customWidgets/__init__.py
@@ -1,6 +1,5 @@
 __version__ = '1.0'
 
-from .ctk_scrollable_dropdown import CTkScrollableDropdown
 from .color_picker import ColorPicker
 from .option_menu import OptionMenu
 from .spinbox import CSpinbox
